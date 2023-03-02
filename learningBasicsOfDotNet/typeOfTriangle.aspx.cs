1648e945b016982e12eb41aaad62f24c297fad4a
Ankur Vijayvargiya <78358127+ankurvijay200@users.noreply.github.com>
2023-03-02 19:48:54 +0530

    initil commit

--- learningBasicsOfDotNet/typeOfTriangle.aspx.cs
+++ learningBasicsOfDotNet/typeOfTriangle.aspx.cs
@@ -9,7 +9,6 @@ public partial class typeOfTriangle : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
