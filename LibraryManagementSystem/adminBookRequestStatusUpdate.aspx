44a679feeccab11d9c2713b60b5056ce6a85cc17
Ankur Vijayvargiya <78358127+ankurvijay200@users.noreply.github.com>
2023-05-13 10:23:39 +0530

    avaulableBook

--- LibraryManagementSystem/adminBookRequestStatusUpdate.aspx
+++ LibraryManagementSystem/adminBookRequestStatusUpdate.aspx
@@ -6,6 +6,15 @@
         {
             color: #FFFFFF;
         }
+        .style6
+        {
+            text-align: center;
+            height: 35px;
+        }
+        .style7
+        {
+            text-align: left;
+        }
     </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
@@ -49,16 +58,25 @@
             <td class="style2" width="50%">
                 <asp:Label ID="Label3" runat="server" Text="Book Id"></asp:Label>
             </td>
-            <td class="style2" width="50%">
-                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <td class="style7" width="50%">
+                <asp:TextBox ID="TextBox1" runat="server" Width="60%"></asp:TextBox>
             </td>
         </tr>
         <tr>
             <td class="style2" width="50%">
                 <asp:Label ID="Label4" runat="server" Text="Price"></asp:Label>
             </td>
-            <td class="style2" width="50%">
-                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <td class="style7" width="50%">
+                <asp:TextBox ID="TextBox2" runat="server" ValidationGroup="a" Width="60%"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+                    ControlToValidate="TextBox2" ErrorMessage="Enter Price" ValidationGroup="a"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td class="style6" colspan="2">
+                <asp:Button ID="Button5" runat="server" onclick="Button5_Click" 
+                    style="background-color: #FFFF00" Text="Add Book" ValidationGroup="a" 
+                    Width="20%" />
             </td>
         </tr>
     </table>
--- LibraryManagementSystem/adminBookRequestStatusUpdate.aspx.cs
+++ LibraryManagementSystem/adminBookRequestStatusUpdate.aspx.cs
@@ -10,12 +10,21 @@ public partial class adminBookRequestStatusUpdate : System.Web.UI.Page
 {
     SqlConnection con;
     SqlCommand cd;
+    SqlDataAdapter da;
     protected void Page_Load(object sender, EventArgs e)
     {
         Label1.Text = Request.QueryString["reqid"].ToString();
         Label2.Text = Request.QueryString["reqbName"].ToString();
         con = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=newTestDatabase;Integrated Security=True");
         con.Open();
+
+        string Nquery = "select max(bId) from LibraryBooks";
+        DataSet ds = new DataSet();
+        da = new SqlDataAdapter(Nquery, con);
+        da.Fill(ds);
+
+        TextBox1.Text = (Convert.ToInt32(ds.Tables[0].Rows[0][0].ToString()) + 1).ToString();
+        Label3.Visible = Label4.Visible = TextBox1.Visible = TextBox2.Visible = Button5.Visible = false;
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
@@ -33,14 +42,22 @@ public partial class adminBookRequestStatusUpdate : System.Web.UI.Page
     }
     protected void Button3_Click(object sender, EventArgs e)
     {
-        string query = "update LibraryBookRequests set reqStatus=" + 2 + " where reqId=" + Convert.ToInt32(Label1.Text);
-        cd = new SqlCommand(query, con);
-        cd.ExecuteNonQuery();
-        Response.Redirect("allBookRequests.aspx");
+        Label3.Visible = Label4.Visible = TextBox1.Visible = TextBox2.Visible = Button5.Visible = true;
     }
     protected void Button4_Click(object sender, EventArgs e)
     {
         string query = "update LibraryBookRequests set reqStatus=" + 3 + " where reqId=" + Convert.ToInt32(Label1.Text);
+        cd = new SqlCommand(query, con);
+        cd.ExecuteNonQuery();
+        Response.Redirect("allBookRequests.aspx");
+    }
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        string querybook = "insert into LibraryBooks values(" + Convert.ToInt32(TextBox1.Text) + ",'" + Label2.Text + "'," + Convert.ToInt32(TextBox2.Text) + "," + 0 + "," + 0 + ",'" + null + "')";
+        cd = new SqlCommand(querybook, con);
+        cd.ExecuteNonQuery();
+
+        string query = "update LibraryBookRequests set reqStatus=" + 2 + " where reqId=" + Convert.ToInt32(Label1.Text);
         cd = new SqlCommand(query, con);
         cd.ExecuteNonQuery();
         Response.Redirect("allBookRequests.aspx");
